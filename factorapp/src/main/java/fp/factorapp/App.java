c478be107622a73b2e0ce3c3f96ff0f758251ec8
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-03-17 17:43:05 +0200

    Aloitettu AKS-algoritmin ohjelmointi

--- factorapp/src/main/java/fp/factorapp/App.java
+++ factorapp/src/main/java/fp/factorapp/App.java
@@ -1,10 +1,16 @@
 
 package fp.factorapp;
 
+import fp.utils.PrimeTools;
+
 public class App {
 
     public static void main(String[] args) {
-    
+        PrimeTools p = new PrimeTools();
+        
+        boolean b = p.aksPrimalityTest(100);
+        
+        System.out.println(b);
     }
     
 }
--- factorapp/src/main/java/fp/utils/PrimeTools.java
+++ factorapp/src/main/java/fp/utils/PrimeTools.java
@@ -4,14 +4,59 @@ import java.math.BigInteger;
 
 public class PrimeTools {
     
-    public boolean primalityTest(int i) {
+    public boolean primalityTest(int n) {
         
-        for (int j = 2; j < Math.sqrt(i); j++) {
-            if(i % j == 0) {
+        for (int j = 2; j < Math.sqrt(n); j++) {
+            if(n % j == 0) {
                 return false;
             }
         }
         
         return true;
+    }
+    /*
+    An implementation of Agrawal-Kayal-Saxena primality test.
+    */
+    public boolean aksPrimalityTest(int n) {
+        
+        /*
+        We check if n is a perfect power.
+        */
+        int a = 1;
+        int b = 1;
+        while(true) {
+            b=1;
+            if(Math.pow(a, b) > n) {
+                break;
+            }
+            while(true) {
+                if (Math.abs((Math.pow(a, b) - n)) < 0.5) {
+                    return false;
+                }
+                else if(Math.pow(a, b) > n) {
+                    break;
+                } else if (b > n) {
+                    break;
+                }
+                b++;
+            }
+            a++;
+        }
+        return true;
+        
+        
+    }
+    
+    /*
+    Needed for the AKS Primality test to work.
+    */
+    public int multiplicativeOrd(int n, int r) {
+        int i = 1;
+        while(true) {
+            if(Math.round(Math.pow(n, i)) % r == 1) {
+                return i;
+            }
+            i++;
+        }
     }
 }
